17d8ab4f09b68bbacefb67176f31768c00b76484
Irina Voronina <irinaworonina@gmail.com>
2022-06-18 23:48:41 +0300

    add my thirteenth c# project

---
+++ Lists/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> fruitSalad = new List<string>();
+
+            fruitSalad.Add("yogurt");
+
+            for(int i =0; i < 3; i++)
+            {
+                Console.WriteLine($"Enter the ingredient {i + 1}:");
+                string userIngridient = Console.ReadLine();
+                fruitSalad.Add(userIngridient);
+            }
+
+            Console.WriteLine($"Your salad consists of: {fruitSalad.Count} ingridients.");
+
+            DisplayIngridients(fruitSalad);
+
+        }
+
+        public static void DisplayIngridients(List<string> someList)
+        {
+            foreach (string ingridient in someList)
+            {
+                Console.WriteLine(ingridient);
+            }
+        }
+
+
+    }
+}
---
+++ ListsAndFiles/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ListsAndFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\Irina\source\repos\Lists\Data\fruit.txt";
+            List<string> fileContent = File.ReadAllLines(filePath).ToList();
+
+            Console.WriteLine("Oroginal file content>");
+            DisplayListElements(fileContent);
+
+            Console.WriteLine("Updated list content");
+            //fileContent.Add("almond");
+            //DisplayListElements(fileContent);
+
+            int indexOfTomato = fileContent.IndexOf("tomato");
+            Console.WriteLine($"Tomato is located at indext {indexOfTomato}");
+            //fileContent.RemoveAt(indexOfTomato);
+            DisplayListElements(fileContent);
+
+            File.WriteAllLines(filePath, fileContent);
+
+
+
+        }
+
+        public static void DisplayListElements(List<string> someList)
+        {
+            foreach(string element in someList)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
